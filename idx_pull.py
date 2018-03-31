9c63a5e9b194d9184b9a4be75937b9ce1ca23274
Saut Daniel Panjaitan <sdanielchp@gmail.com>
2018-03-31 10:34:53 +0700

    update

--- idx_pull.py
+++ idx_pull.py
@@ -3,7 +3,27 @@ from selenium.webdriver.support.ui import Select
 import time
 import urllib
 import sys
+import MySQLdb
+import sys
  
+# open a database connection
+# be sure to change the host IP address, username, password and database name to match your own
+connection = MySQLdb.connect (host = "localhost", user = "root", passwd = "satunol10", db = "Stock")
+# prepare a cursor object using cursor() method
+cursor = connection.cursor ()
+# execute the SQL query using execute() method.
+cursor.execute ("select * from Daftar_Perusahaan")
+listCompany=[]
+# fetch all of the rows from the query
+data = cursor.fetchall ()
+# print the rows
+for row in data :
+	#print row[0], row[1]
+	listCompany.append(row[1])
+# close the cursor object
+cursor.close ()
+# close the connection
+connection.close ()
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
@@ -13,48 +33,49 @@ driver = webdriver.Chrome("/home/daniel/tools_embedded/program_python/chromedriv
 driver.get('http://www.idx.co.id/id-id/beranda/perusahaantercatat/laporankeuangandantahunan.aspx')
 years = ['2009','2010','2011','2012','2013','2014','2015','2016']
 triwulans = ['Triwulan I','Triwulan II','Triwulan III']
-for year in years:
+for listComp in listCompany:
+	for year in years:
 	
-	for triwulan in triwulans:
-		# click radio button
-		python_button = driver.find_elements_by_xpath("//input[@name='dnn$ctr518$MainView$FR' and @value='rbFinancialReport']")[0]
-		python_button.click()
+		for triwulan in triwulans:
+			# click radio button
+			python_button = driver.find_elements_by_xpath("//input[@name='dnn$ctr518$MainView$FR' and @value='rbFinancialReport']")[0]
+			python_button.click()
  
-		# type text
-		text_area = driver.find_element_by_id('dnn_ctr518_MainView_cbCODE_Input')
-		#text_area.send_keys("print('AALI')")
-		text_area.clear()
-		text_area.send_keys("AALI")
 
-		# Drop down menu
-		select = Select(driver.find_element_by_id('dnn_ctr518_MainView_ddlYearCalendar'))
+			# Drop down menu
+			select = Select(driver.find_element_by_id('dnn_ctr518_MainView_ddlYearCalendar'))
 
-		# select by visible text
-		#select.select_by_visible_text('2017')
-		select.select_by_visible_text(year)
+			# select by visible text
+			#select.select_by_visible_text('2017')
+			select.select_by_visible_text(year)
 
-		# Drop down menu
-		select = Select(driver.find_element_by_id('dnn_ctr518_MainView_ddlPeriod'))
+			# Drop down menu
+			select = Select(driver.find_element_by_id('dnn_ctr518_MainView_ddlPeriod'))
 
-		# select by visible text
-		select.select_by_visible_text(triwulan)
+			# select by visible text
+			select.select_by_visible_text(triwulan)
 
+			# type text
+			text_area = driver.find_element_by_id('dnn_ctr518_MainView_cbCODE_Input')
+			#text_area.send_keys("print('AALI')")
+			text_area.clear()
+			text_area.send_keys(listComp)
  
-		# click submit button
-		#submit_button = driver.find_elements_by_xpath('//*[@id="editor"]/table/tbody/tr[3]/td/table/tbody/tr/td/div/table/tbody/tr/td[3]/input')[0]
-		submit_button = driver.find_elements_by_id('dnn_ctr518_MainView_lbSearch')[0]
-		submit_button.click()
-		time.sleep(10)
+			# click submit button
+			#submit_button = driver.find_elements_by_xpath('//*[@id="editor"]/table/tbody/tr[3]/td/table/tbody/tr/td/div/table/tbody/tr/td[3]/input')[0]
+			submit_button = driver.find_elements_by_id('dnn_ctr518_MainView_lbSearch')[0]
+			submit_button.click()
+			time.sleep(10)
 
-		# click link
-		#link = driver.find_element_by_link_text('http://www.idx.co.id/Portals/0/StaticData/ListedCompanies/Corporate_Actions/New_Info_JSX/Jenis_Informasi/01_Laporan_Keuangan/02_Soft_Copy_Laporan_Keuangan\Laporan%20Keuangan%20Tahun%202017\TW1\AALI\FinancialStatement-2017-I-AALI.xlsx')
-		try:
-			link = driver.find_element_by_id('dnn_ctr518_MainView_lvSearchingResult_ctrl0_rptMain_ctl00_hl')
-			print(link.get_attribute('href'))
-			urllib.urlretrieve(link.get_attribute('href'), year + "_" + triwulan + "_laporan1.pdf")
-		except Exception as ex:
-			print "Unexpected error:", sys.exc_info()[0]
-		#html = response.read()
-		#link.click()
+			# click link
+			#link = driver.find_element_by_link_text('http://www.idx.co.id/Portals/0/StaticData/ListedCompanies/Corporate_Actions/New_Info_JSX/Jenis_Informasi/01_Laporan_Keuangan/02_Soft_Copy_Laporan_Keuangan\Laporan%20Keuangan%20Tahun%202017\TW1\AALI\FinancialStatement-2017-I-AALI.xlsx')
+			try:
+				link = driver.find_element_by_id('dnn_ctr518_MainView_lvSearchingResult_ctrl0_rptMain_ctl00_hl')
+				print(link.get_attribute('href'))
+				urllib.urlretrieve(link.get_attribute('href'), year + "_" + triwulan + "_laporan1.pdf")
+			except Exception as ex:
+				print "Unexpected error:", sys.exc_info()[0]
+			#html = response.read()
+			#link.click()
 
 
